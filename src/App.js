e23fd89cbcca06612fea6d43378a93423c65776a
BytesAsArt <bytesasart@localhost-live.baa-network>
2024-04-08 18:43:14 +0200

    added router, structural changes

--- src/App.js
+++ src/App.js
@@ -1,114 +1,17 @@
-import { useEffect, useMemo, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import './App.css';
-import { loadSlim } from "@tsparticles/slim"; 
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
+//import SkillsPage from './SkillsPage';
+//import AboutPage from './AboutPage';
+import WIPPage from './components/WIPPage';
 
 const App = () => {
-  const [init, setInit] = useState(false);
-
-  // this should be run only once per application lifetime
-  useEffect(() => {
-    initParticlesEngine(async (engine) => {
-
-      await loadSlim(engine);
-      
-    }).then(() => {
-      setInit(true);
-    });
-  }, []);
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
-
-  const options = useMemo(
-    () => ({
-      background: {
-        color: {
-          value: "#000000",
-        },
-      },
-      fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: true,
-            mode: "push",
-          },
-          onHover: {
-            enable: true,
-            mode: "repulse",
-          },
-        },
-        modes: {
-          push: {
-            quantity: 4,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
-        },
-      },
-      particles: {
-        color: {
-          value: "#ffffff",
-        },
-        links: {
-          color: "#ffffff",
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-        },
-        move: {
-          direction: "none",
-          enable: true,
-          outModes: {
-            default: "bounce",
-          },
-          random: false,
-          speed: 6,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-          },
-          value: 80,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          type: "circle",
-        },
-        size: {
-          value: { min: 1, max: 5 },
-        },
-      },
-      detectRetina: true,
-    }),
-    [],
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/wip" element={<WIPPage />} />
+    </Routes>
   );
+};
 
-  if (init) {
-    return (
-      <>
-        <Particles
-          id="tsparticles"
-          particlesLoaded={particlesLoaded}
-          options={options}
-        />
-        <div className="wip-content">
-          <h2 className="wip-maintext">BytesAsArt</h2>
-        </div>
-      </>
-    );
-
-
-
-  }
-}
-
-  export  default App;
+export default App;
